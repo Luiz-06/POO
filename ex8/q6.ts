25fb54ac1bdec00f5245c9b6c81dba0679aa7971
Luiz-06 <160617306+Luiz-06@users.noreply.github.com>
2024-12-16 09:03:15 -0300

    Update q6.ts

--- ex8/q6.ts
+++ ex8/q6.ts
@@ -1,1 +1,16 @@
-//Não consegui 
+class App {
+    iniciar(): void{
+        const opcao = null
+        do {
+            console.log('\nBem-vindo! Escolha uma opção:');
+            console.log('Contas:');
+            console.log('1 - Inserir    2 - Consultar  3 - Sacar');
+            console.log('4 - Depositar  5 - Excluir  6 - Transferir');
+            console.log('7 - Totalizações');
+            console.log('Clientes:');
+            console.log('8 - Inserir    9 - Consultar   10 - Associar');
+            console.log('11 - Total aplicado por cliente');
+            console.log('0 - Sair');
+        } while (opcao != "0");
+    }
+}
